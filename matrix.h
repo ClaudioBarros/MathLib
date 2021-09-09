3594098b49de6a3d596b0187663e3df8e00ae46f
claudio <claudiobarros256@gmail.com>
2021-09-09 12:34:52 -0300

    add mat4 class

---
+++ matrix.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "vectors.h"
+
+/*
+TODO:
+-mat3
+-AVX, AVX2 Support
+*/
+
+struct mat4
+{
+	float4 row[4];
+
+	//constructors
+	explicit mat4(const float value = 0.0f)
+	{
+		row[0] = float4(1.0f, 0.0f, 0.0f, 0.0f);
+		row[1] = float4(0.0f, 1.0f, 0.0f, 0.0f);
+		row[2] = float4(0.0f, 0.0f, 1.0f, 0.0f);
+		row[3] = float4(0.0f, 0.0f, 0.0f, 1.0f);
+	}
+
+	explicit mat4(float4 r0, float4 r1, float4 r2, float4 r3)
+	{
+		row[0] = r0;
+		row[1] = r1; 
+		row[2] = r2; 
+		row[3] = r3; 
+	}
+
+	explicit mat4(float *p)
+	{
+		int index = 0;
+		for(int i = 0; i < 4; i++)
+		{
+			row[i] = float4(p[index + 0], p[index + 1], p[index + 2], p[index + 3]);
+			index += 4;
+		}
+	}
+
+	//multidimensional array access operators: mat4(row, column)
+	VM_INLINE float  operator() (size_t r, size_t c = 0) const {return row[r].x();};
+	VM_INLINE float  operator() (size_t r, size_t c = 1) const {return row[r].y();};
+	VM_INLINE float  operator() (size_t r, size_t c = 2) const {return row[r].z();};
+	VM_INLINE float  operator() (size_t r, size_t c = 3) const {return row[r].w();};
+
+	VM_INLINE float&  operator() (size_t r, size_t c) {return row[r][c];};
+};
+
+//mat4 operations
+//because we're using _vectorcall, we don't need to use references/pointers
+
+VM_INLINE mat4 operator+ (mat4 a, mat4 b) 
+{
+	a.row[0] = a.row[0] + b.row[0];
+	a.row[1] = a.row[1] + b.row[1];
+	a.row[2] = a.row[2] + b.row[2];
+	a.row[3] = a.row[3] + b.row[3];
+	
+	return a;
+}
+
+VM_INLINE mat4 operator- (mat4 a, mat4 b) 
+{
+	a.row[0] = a.row[0] - b.row[0];
+	a.row[1] = a.row[1] - b.row[1];
+	a.row[2] = a.row[2] - b.row[2];
+	a.row[3] = a.row[3] - b.row[3];
+	
+	return a;
+}
+
+//linear combination: row-vector * matrix
+VM_INLINE float4 operator* (float4 a, mat4 b)
+{
+	float4 result;	
+	result = a.x() * b.row[0];
+	result += a.y() * b.row[1];
+	result += a.z() * b.row[2];
+	result += a.w() * b.row[3];
+	
+	return result;
+}
+
+VM_INLINE mat4 operator* (mat4 a, mat4 b) 
+{
+	a.row[0] = a.row[0] * b;
+	a.row[1] = a.row[1] * b;
+	a.row[2] = a.row[2] * b;
+	a.row[3] = a.row[3] * b;
+	
+	return a;
+}
+	
--- vectors.h
+++ vectors.h
@@ -18,9 +18,20 @@
 //Examples: SHUFFLE3(v, 0,1,2) leaves the vector unchanged.
 //          SHUFFLE3(v, 0,0,0) replicates the X coordinate throughout the vector.
 
-typedef float3 bool2; //SIMD comparison operators return multiple values;
-typedef float3 bool3; //SIMD comparison operators return multiple values;
-typedef float4 bool4; //SIMD comparison operators return multiple values;
+// Comparison operators need to return a SIMD bool.
+typedef float3 bool2; 
+typedef float3 bool3; 
+typedef float4 bool4; 
+
+// Helpers for initializing static data.
+#define VCONST extern const __declspec(selectany)
+struct vconstu
+{
+	union { uint32_t u[4]; __m128 v; };
+	inline operator __m128() const { return v; }
+};
+
+VCONST vconstu vsignbits = { 0x80000000, 0x80000000, 0x80000000, 0x80000000 };
 
 //-------------------- FLOAT2 ------------------------------
 struct float2
@@ -64,12 +75,12 @@ VM_INLINE float2 float2i(int x, int y) { return float2((float)x, (float)y); }
 //float2 operators:
 VM_INLINE float2  operator+ (float2 a, float2 b) {a.m = _mm_add_ps(a.m, b.m); return a;} 
 VM_INLINE float2  operator- (float2 a, float2 b) {a.m = _mm_sub_ps(a.m, b.m); return a;}
-VM_INLINE float2  operator* (float2 a, float2 b) {a.m = _mm_mul_ps(a.m, b.m); return a;}; 
-VM_INLINE float2  operator/ (float2 a, float2 b) {a.m = _mm_div_ps(a.m, b.m); return a;}; 
-VM_INLINE float2  operator* (float2 a, float  b) {a.m = _mm_mul_ps(a.m, _mm_set1_ps(b)); return a;}; 
-VM_INLINE float2  operator/ (float2 a, float  b) {a.m = _mm_div_ps(a.m, _mm_set1_ps(b)); return a;}; 
-VM_INLINE float2  operator* (float  a, float2 b) {b.m = _mm_mul_ps(_mm_set1_ps(a), b.m); return b;}; 
-VM_INLINE float2  operator/ (float  a, float2 b) {b.m = _mm_div_ps(_mm_set1_ps(a), b.m); return b;}; 
+VM_INLINE float2  operator* (float2 a, float2 b) {a.m = _mm_mul_ps(a.m, b.m); return a;}
+VM_INLINE float2  operator/ (float2 a, float2 b) {a.m = _mm_div_ps(a.m, b.m); return a;}
+VM_INLINE float2  operator* (float2 a, float  b) {a.m = _mm_mul_ps(a.m, _mm_set1_ps(b)); return a;}
+VM_INLINE float2  operator/ (float2 a, float  b) {a.m = _mm_div_ps(a.m, _mm_set1_ps(b)); return a;}
+VM_INLINE float2  operator* (float  a, float2 b) {b.m = _mm_mul_ps(_mm_set1_ps(a), b.m); return b;}
+VM_INLINE float2  operator/ (float  a, float2 b) {b.m = _mm_div_ps(_mm_set1_ps(a), b.m); return b;}
 VM_INLINE float2& operator+= (float2 &a, float2 b) {a = a + b; return a;}
 VM_INLINE float2& operator-= (float2 &a, float2 b) {a = a - b; return a;} 
 VM_INLINE float2& operator*= (float2 &a, float2 b) {a = a * b; return a;}
@@ -91,11 +102,10 @@ struct float3
 	__m128 m;
 
 	VM_INLINE float3(){}	
-
 	VM_INLINE explicit float3(const float *p){ m = _mm_set_ps(p[2], p[2], p[1], p[0]); }
 	VM_INLINE explicit float3(float x, float y, float z) { m = _mm_set_ps(z, z, y, x); }
 	VM_INLINE explicit float3(__m128 v) { m = v; }
-
+	VM_INLINE explicit float3(float2 v, float z) {m = _mm_add_ps(v.m, _mm_set_ps(0.0f, 0.0f, z , 0.0f));}
 
 	VM_INLINE float x() const { return _mm_cvtss_f32(m); }
 	VM_INLINE float y() const { return _mm_cvtss_f32(_mm_shuffle_ps(m, m, _MM_SHUFFLE(1, 1, 1, 1))); }
@@ -129,17 +139,18 @@ struct float3
 	VM_INLINE float& operator[] (size_t i) { return m.m128_f32[i]; };
 };
 
+//Constructor from 3 ints to avoid manual casting
 VM_INLINE float3 float3i(int x, int y, int z) { return float3((float)x, (float)y, (float)z);}
 
-//float3 operators:
+//basic binary operators:
 VM_INLINE float3  operator+ (float3 a, float3 b) {a.m = _mm_add_ps(a.m, b.m); return a;} 
 VM_INLINE float3  operator- (float3 a, float3 b) {a.m = _mm_sub_ps(a.m, b.m); return a;}
-VM_INLINE float3  operator* (float3 a, float3 b) {a.m = _mm_mul_ps(a.m, b.m); return a;}; 
-VM_INLINE float3  operator/ (float3 a, float3 b) {a.m = _mm_div_ps(a.m, b.m); return a;}; 
-VM_INLINE float3  operator* (float3 a, float  b) {a.m = _mm_mul_ps(a.m, _mm_set1_ps(b)); return a;}; 
-VM_INLINE float3  operator/ (float3 a, float  b) {a.m = _mm_div_ps(a.m, _mm_set1_ps(b)); return a;}; 
-VM_INLINE float3  operator* (float  a, float3 b) {b.m = _mm_mul_ps(_mm_set1_ps(a), b.m); return b;}; 
-VM_INLINE float3  operator/ (float  a, float3 b) {b.m = _mm_div_ps(_mm_set1_ps(a), b.m); return b;}; 
+VM_INLINE float3  operator* (float3 a, float3 b) {a.m = _mm_mul_ps(a.m, b.m); return a;}
+VM_INLINE float3  operator/ (float3 a, float3 b) {a.m = _mm_div_ps(a.m, b.m); return a;}
+VM_INLINE float3  operator* (float3 a, float  b) {a.m = _mm_mul_ps(a.m, _mm_set1_ps(b)); return a;}
+VM_INLINE float3  operator/ (float3 a, float  b) {a.m = _mm_div_ps(a.m, _mm_set1_ps(b)); return a;}
+VM_INLINE float3  operator* (float  a, float3 b) {b.m = _mm_mul_ps(_mm_set1_ps(a), b.m); return b;}
+VM_INLINE float3  operator/ (float  a, float3 b) {b.m = _mm_div_ps(_mm_set1_ps(a), b.m); return b;}
 VM_INLINE float3& operator+= (float3 &a, float3 b) {a = a + b; return a;}
 VM_INLINE float3& operator-= (float3 &a, float3 b) {a = a - b; return a;} 
 VM_INLINE float3& operator*= (float3 &a, float3 b) {a = a * b; return a;}
@@ -155,6 +166,44 @@ VM_INLINE bool3   operator>= (float3 a, float3 b) {a.m = _mm_cmpge_ps(a.m, b.m);
 VM_INLINE float3  min(float3 a, float3 b) {a.m = _mm_min_ps(a.m, b.m); return a;}
 VM_INLINE float3  max(float3 a, float3 b) {a.m = _mm_max_ps(a.m, b.m); return a;}
 
+// Unary operators.
+VM_INLINE float3 operator- (float3 a) { return float3(_mm_setzero_ps()) - a; }
+VM_INLINE float3 abs(float3 v) { v.m = _mm_andnot_ps(vsignbits, v.m); return v; }
+
+// Horizontal min/max.
+VM_INLINE float hmin(float3 v) {
+	v = min(v, SHUFFLE3(v, 1, 0, 2));
+	return min(v, SHUFFLE3(v, 2, 0, 1)).x();
+}
+
+VM_INLINE float hmax(float3 v) {
+	v = max(v, SHUFFLE3(v, 1, 0, 2));
+	return max(v, SHUFFLE3(v, 2, 0, 1)).x();
+}
+
+// 3D cross product.
+VM_INLINE float3 cross(float3 a, float3 b) {
+	// x  <-  a.y*b.z - a.z*b.y
+	// y  <-  a.z*b.x - a.x*b.z
+	// z  <-  a.x*b.y - a.y*b.x
+	// We can save a shuffle by grouping it in this wacky order:
+	return (a.zxy()*b - a*b.zxy()).zxy();
+}
+
+// Returns a 3-bit code where bit0..bit2 is X..Z
+VM_INLINE unsigned mask(float3 v) { return _mm_movemask_ps(v.m) & 7; }
+
+// Once we have a comparison, we can branch based on its results:
+VM_INLINE bool any(bool3 v) { return mask(v) != 0; }
+VM_INLINE bool all(bool3 v) { return mask(v) == 7; }
+
+VM_INLINE float3 clamp(float3 t, float3 a, float3 b) { return min(max(t, a), b); }
+VM_INLINE float sum(float3 v) { return v.x() + v.y() + v.z(); }
+VM_INLINE float dot(float3 a, float3 b) { return sum(a*b); }
+VM_INLINE float length(float3 v) { return sqrtf(dot(v, v)); }
+VM_INLINE float lengthSq(float3 v) { return dot(v, v); }
+VM_INLINE float3 normalize(float3 v) { return v * (1.0f / length(v)); }
+VM_INLINE float3 lerp(float3 a, float3 b, float t) { return a + (b-a)*t; }
 
 //-------------- FLOAT4 ----------------------
 struct float4
@@ -166,6 +215,10 @@ struct float4
 	VM_INLINE explicit float4(const float *p){ m = _mm_set_ps(p[3], p[2], p[1], p[0]); }
 	VM_INLINE explicit float4(float x, float y, float z, float w) { m = _mm_set_ps(w, z, y, x); }
 	VM_INLINE explicit float4(__m128 v) { m = v; }
+	VM_INLINE explicit float4(float2 v) { m = v.m;}
+	VM_INLINE explicit float4(float2 v, float z, float w) {m = _mm_add_ps(v.m, _mm_set_ps(0.0f, 0.0f, z , w));}
+	VM_INLINE explicit float4(float3 v) { m = v.m;}
+	VM_INLINE explicit float4(float3 v, float w) { m = _mm_add_ps(v.m, _mm_set_ps(0.0f, 0.0f, 0.0f, w));}
 
 
 	VM_INLINE float x() const { return _mm_cvtss_f32(m); }
@@ -215,12 +268,12 @@ VM_INLINE float4 float4i(int x, int y, int z) { return float3((float)x, (float)y
 //float4 operators:
 VM_INLINE float4  operator+ (float4 a, float4 b) {a.m = _mm_add_ps(a.m, b.m); return a;} 
 VM_INLINE float4  operator- (float4 a, float4 b) {a.m = _mm_sub_ps(a.m, b.m); return a;}
-VM_INLINE float4  operator* (float4 a, float4 b) {a.m = _mm_mul_ps(a.m, b.m); return a;}; 
-VM_INLINE float4  operator/ (float4 a, float4 b) {a.m = _mm_div_ps(a.m, b.m); return a;}; 
-VM_INLINE float4  operator* (float4 a, float  b) {a.m = _mm_mul_ps(a.m, _mm_set1_ps(b)); return a;}; 
-VM_INLINE float4  operator/ (float4 a, float  b) {a.m = _mm_div_ps(a.m, _mm_set1_ps(b)); return a;}; 
-VM_INLINE float4  operator* (float  a, float4 b) {b.m = _mm_mul_ps(_mm_set1_ps(a), b.m); return b;}; 
-VM_INLINE float4  operator/ (float  a, float4 b) {b.m = _mm_div_ps(_mm_set1_ps(a), b.m); return b;}; 
+VM_INLINE float4  operator* (float4 a, float4 b) {a.m = _mm_mul_ps(a.m, b.m); return a;}
+VM_INLINE float4  operator/ (float4 a, float4 b) {a.m = _mm_div_ps(a.m, b.m); return a;}
+VM_INLINE float4  operator* (float4 a, float  b) {a.m = _mm_mul_ps(a.m, _mm_set1_ps(b)); return a;} 
+VM_INLINE float4  operator/ (float4 a, float  b) {a.m = _mm_div_ps(a.m, _mm_set1_ps(b)); return a;}
+VM_INLINE float4  operator* (float  a, float4 b) {b.m = _mm_mul_ps(_mm_set1_ps(a), b.m); return b;}
+VM_INLINE float4  operator/ (float  a, float4 b) {b.m = _mm_div_ps(_mm_set1_ps(a), b.m); return b;}
 VM_INLINE float4& operator+= (float4 &a, float4 b) {a = a + b; return a;}
 VM_INLINE float4& operator-= (float4 &a, float4 b) {a = a - b; return a;} 
 VM_INLINE float4& operator*= (float4 &a, float4 b) {a = a * b; return a;}
@@ -236,3 +289,30 @@ VM_INLINE bool4   operator>= (float4 a, float4 b) {a.m = _mm_cmpge_ps(a.m, b.m);
 VM_INLINE float4  min(float4 a, float4 b) {a.m = _mm_min_ps(a.m, b.m); return a;}
 VM_INLINE float4  max(float4 a, float4 b) {a.m = _mm_max_ps(a.m, b.m); return a;}
 
+// Unary operators.
+VM_INLINE float4 operator- (float4 a) { return float4(_mm_setzero_ps()) - a; }
+VM_INLINE float4 abs(float4 v) { v.m = _mm_andnot_ps(vsignbits, v.m); return v; }
+
+// Horizontal min/max.
+VM_INLINE float hmin(float4 v) {
+//TODO
+}
+
+VM_INLINE float hmax(float4 v) {
+//TODO
+}
+
+// Returns a 3-bit code where bit0..bit2 is X..Z
+VM_INLINE unsigned mask(float4 v) {}
+
+// Once we have a comparison, we can branch based on its results:
+VM_INLINE bool any(bool4 v) { return mask(v) != 0; }
+VM_INLINE bool all(bool4 v) { return mask(v) == 7; }
+
+VM_INLINE float4 clamp(float4 t, float4 a, float4 b) { return min(max(t, a), b); }
+VM_INLINE float sum(float4 v) { return v.x() + v.y() + v.z() + v.w(); }
+VM_INLINE float dot(float4 a, float4 b) { return sum(a*b); }
+VM_INLINE float length(float4 v) { return sqrtf(dot(v, v)); }
+VM_INLINE float lengthSq(float4 v) { return dot(v, v); }
+VM_INLINE float4 normalize(float4 v) { return v * (1.0f / length(v)); }
+VM_INLINE float4 lerp(float4 a, float4 b, float t) { return a + (b-a)*t; }
